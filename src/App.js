2a385a611c649f3d4adbd63b83065c1f62995c1b
Ashlee Hayes <hayesashlee88@gmail.com>
2020-01-21 13:48:42 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from './Components/NavBar'
+import CourseList from './Components/CourseList'
+
+
+function App() {
+  return (
+    <div className="App">
+ <NavBar />
+ <CourseList />
+    </div>
+  );
+}
+
+export default App;
